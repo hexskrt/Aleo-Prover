ff41769b254832f3304b74b99cafde9d33c3704d
hexskrt <90503141+hexskrt@users.noreply.github.com>
2022-11-14 13:45:15 +0700

    Update prover.sh

--- prover.sh
+++ prover.sh
@@ -28,9 +28,6 @@ sudo apt-get update && sudo apt install git && sudo apt install screen
 echo -e "\e[1m\e[32m2. Open Port... \e[0m" && sleep 1
 ufw allow 22 && ufw allow 4133/tcp && ufw allow 3033/tcp && ufw enable
 
-echo -e "\e[1m\e[32m3. Install Rupstup... \e[0m" && sleep 1
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-
 echo -e "\e[1m\e[32m4. Clone Repository... \e[0m" && sleep 1
 cd $HOME
 git clone https://github.com/AleoHQ/snarkOS.git --depth 1
@@ -45,5 +42,8 @@ curl https://sh.rustup.rs -sSf | sh
 source "$HOME/.cargo/env"
 cargo install cargo --force
 cargo install --path .
+
+echo -e "\e[1m\e[32m7. Create Account Aleo Prover... \e[0m" && sleep 1
+snarkos account new
 
 echo '=============== SETUP FINISHED ==================='
